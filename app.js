d5e4cdd91e6af128a5d45eba6cee44bb40bb8448
Jason <user@email.com>
2021-12-23 03:56:34 -0500

    Commit 5 - Sum Array

--- app.js
+++ app.js
@@ -70,6 +70,19 @@ function sumArray (array) {
    
 console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
+// ## 6. Prime Numbers
+// A Prime number is a number that is not evenly divisible by another number except 1 and itself. If you want to read more deeply about it, [go here](https://en.wikipedia.org/wiki/Prime_number).
+// To test whether a number is Prime, you only need to test as far as the **square root** of that number. This is advisable for optimization and testing large numbers.
+
+// ### Step One
+// Write a function called `checkPrime` that will test whether a number is Prime. The function will return true (Boolean) if Prime, false if not.
+// _Hint:_ Check every number up to the square root. To do this, try a _for loop_.
+
+// ### Step Two
+// Write another function called `printPrimes` that will print (console log) all the Primes up to an arbitrary limit. For example, if you invoke your function with `printPrimes(97)`, it will print all the Prime numbers up to and including 97.
+// This function can **call on** the previous `checkPrime` function.
+// </details>
+
 
 
 
